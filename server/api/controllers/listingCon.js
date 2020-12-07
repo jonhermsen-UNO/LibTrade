525bd970469e46a99e27484e87066e3daefa2a9f
cyberboy1551 <blacy1551@gmail.com>
2020-12-06 22:55:33 -0600

    moved bookController per specifications and added findBookByISBN

--- server/api/controllers/listingCon.js
+++ server/api/controllers/listingCon.js
@@ -2,6 +2,7 @@ const accountModel = require('../models/accountMod')
 const collegeModel = require('../models/collegeMod')
 const listingModel = require('../models/listingMod')
 const passport = require('passport')
+const axios = require('axios').default;
 const controller = {}
 
 //gets listing form to create listing
@@ -50,5 +51,37 @@ controller.postListing = function(req, res){
 controller.removeListing = function(req, res){
   res.send("viewListing not yet implemented (POST)");
 };
+
+
+//=========BOOKS=========
+
+//Finds book from google books api by book id
+controller.findBookById = (req, res) => {
+  const URI = `https://www.googleapis.com/books/v1/volumes/${req.body.searchQuery}`;
+  axios.get(URI, {responseType: "json", method:"get"}).then((data) => {
+      res.send(data.data); 
+      //TODO: Still need to format it into a book object
+  }).catch((err) => {
+      console.log(err);
+      res.send(`Error: ${err}`)
+  })
+
+}
+//Searchs for book with name matching search query.
+//Returns book array where searchquery exists in name anywhere
+controller.findBookByISBN = (req, res) => {
+  const URI = `https://www.googleapis.com/books/v1/volumes?q=isbn:${req.body.searchQuery}`;
+  axios.get(URI, {responseType: "json", method:"get"}).then((data) => {
+      res.send(data.data.items);
+      //TODO: Still need to format it into a book object
+  }).catch((err) => {
+      res.send(`Error: ${err}`)
+  });
+}
+
+//keys.google.apiKey is not being used here due to 503 error
+//It still works but I'll look into it.
+//The key would be appended at the end of the URI:
+//  &key=API KEY HERE
 
 module.exports = controller
