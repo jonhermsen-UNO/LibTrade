b44379f3ec87a519b3c1b46465cda586b7680077
jonhermsen-UNO <jonhermsen@unomaha.edu>
2020-12-12 00:26:36 -0600

    refactor: sync Account model with ER diagram

--- server/api/models/accountMod.js
+++ server/api/models/accountMod.js
@@ -1,11 +1,19 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const db = require('../../lib/dbconn')
 const sequelize = new Sequelize(db);
-const model = sequelize.define('User', {
-  username: DataTypes.STRING,
-  password: DataTypes.STRING,
-  email: DataTypes.STRING,
-  collegeID: DataTypes.STRING
+
+const model = sequelize.define('Account', {
+  AccountID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  CollegeID: DataTypes.INTEGER,
+  Username: DataTypes.STRING,
+  Password: DataTypes.STRING,
+  Email: DataTypes.STRING,
+}, {
+  timestamps: false,
+  tableName: 'Account'
 });
 
 module.exports = model
