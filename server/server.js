1ef523d792f4b5e8d4fb832cbfb0b8913972a8a4
jonhermsen-UNO <jonhermsen@unomaha.edu>
2020-12-14 01:07:49 -0600

    build: move passport setup to util folder

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const passport = require('passport')
-const passportSetup = require('./config/passport-setup')
+const passportSetup = require('./util/passport')
 const express = require('express')
 const app = express()
 const session = require('express-session')
