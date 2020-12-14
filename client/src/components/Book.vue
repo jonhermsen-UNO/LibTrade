9e31e2539223089cd8a298d05b401dc90b8dba1f
jonhermsen-UNO <jonhermsen@unomaha.edu>
2020-12-14 00:22:36 -0600

    style: improve whitespace on mobile for books

--- client/src/components/Book.vue
+++ client/src/components/Book.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <b-row>
-            <b-col cols="4" class="text-right">
+            <b-col cols="12" sm="4" class="text-center text-sm-right">
                 <b-img v-if="bookData.ThumbnailURL" :src=bookData.ThumbnailURL fluid/>
             </b-col>
-            <b-col cols="8" class="text-left">
+            <b-col cols="12" sm="8" class="text-left">
                 <h2 v-if="bookData.Title" class="text-primary">{{bookData.Title}}</h2>
                 <div v-if="bookData.Publisher && bookData.PublishYear" class="lead">{{bookData.Publisher}}, {{bookData.PublishYear}}</div>
                 <div>{{bookData.Authors}}<br>
